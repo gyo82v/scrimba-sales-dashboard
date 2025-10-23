8064710008974ebbfa678b0ae1fa9ed102a376d8
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-10-23 15:55:54 +0200

    feat: add fetchdata in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
+import {useState, useEffect} from "react"
+import supabase from "./supabase-client.js"
+
 import Header from "./components/Header"
+import Dashboard from "./components/Dashboard"
+import Form from "./components/Form"
 
 
 function App() {
-  const container = `bg-indigo-50`
+  const [metrics, setMetrics] = useState([])
+  const container = `bg-indigo-50 flex flex-col`
+
+  useEffect(() => {
+    const fetchMetrics = async () => {
+      try{
+        const {error, data} = await supabase.from('sales_deals').select(`name, value.sum()`)
+        console.log("res: ", data)
+        if(error) throw error
+        setMetrics(data)
+      }catch(err){
+        console.error("Error GET fecthMetrics", err)
+      }
+    }
+
+    fetchMetrics()
+  }, [])
+
+
 
 
   return (
     <div className={container}>
       <Header />
+      <Dashboard metrics={metrics} />
+      <Form metrics={metrics} />
     </div>
   )
 }
