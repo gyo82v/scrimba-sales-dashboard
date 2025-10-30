c5fab2badc45ee35a678d1f95b83b8e771a47d67
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-10-30 18:11:57 +0100

    feat: add logout functioality to the Header component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,16 @@
-import {NavLink} from "react-router-dom"
+import {NavLink, useNavigate} from "react-router-dom"
+import { FaSignOutAlt } from "react-icons/fa";
+import { useAuth } from "../../context/AuthContext";
+import {useState} from "react"
 
 export default function Header(){
+    const {users, session, signOut} = useAuth()
+    const [error, seterror] = useState(null)
+    const navigate = useNavigate()
+    const currentUser = users.find(u => u.id === session?.user?.id)
+    console.log("Current user: ", currentUser)
+
+    //tailwind
     const header = `bg-gradient-to-br from-indigo-700 to-indigo-600 text-indigo-100 
                     p-5 shadow-lg shadow-indigo-700/30 
                     text-2xl font-bold w-full flex items-center justify-between 
@@ -9,10 +19,29 @@ export default function Header(){
     const link = `text-sm md:text-lg font-semibold 
                   transition-transform transition-colors duration-300 ease-in-out 
                   hover:scale-105 active:scale-95 hover:text-indigo-300`
+    const section = `text-sm flex items-center font-medium gap-6`
+    const span = `text-rose-300 font-bold ml-1`
+    const btn = `transition-transform hover:scale-120 active:scale-95`
+    //
+
+    const handleLogOut = async () => {
+        const {success, error} = await signOut()
+        if(error) seterror(error.message)
+        if(success) navigate("/signin")
+    }
     return(
         <header className={header}>
             <h1>Sales Team</h1>
-            <nav className={nav}>
+            {session ? 
+             <section className={section}>
+                <p>Logged in as: 
+                    <span className={span}>{currentUser?.name},
+                        <span className="text-indigo-200 ml-1 font-small">{currentUser?.account_type}</span>
+                    </span>
+                </p>
+                <button className={btn} onClick={handleLogOut}><FaSignOutAlt className="h-5 w-5" /></button>
+             </section> :
+             <nav className={nav}>
                 <NavLink 
                   to="dashboard" 
                   className={({isActive}) => isActive ? `${link} underline` : link}
@@ -26,6 +55,7 @@ export default function Header(){
                     Sign in
                 </NavLink>
             </nav>
+            }
         </header>
     )
 }
