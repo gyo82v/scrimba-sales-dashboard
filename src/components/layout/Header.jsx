73fd021c9358f82e28d20340cf431901191ac8dc
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-10-31 14:31:51 +0100

    refactor: clean up styles and removed unused code

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,32 +1,21 @@
-import {NavLink, useNavigate} from "react-router-dom"
+import {useNavigate} from "react-router-dom"
 import { FaSignOutAlt } from "react-icons/fa";
 import { useAuth } from "../../context/AuthContext";
-import {useState} from "react"
 
 export default function Header(){
     const {users, session, signOut} = useAuth()
-    const [error, seterror] = useState(null)
     const navigate = useNavigate()
     const currentUser = users.find(u => u.id === session?.user?.id)
     console.log("Current user: ", currentUser)
 
     //tailwind
     const header = `bg-gradient-to-br from-indigo-700 to-indigo-600 text-indigo-100 
-                    p-5 shadow-lg shadow-indigo-700/30 
-                    text-2xl font-bold w-full flex items-center justify-between 
-                    md:text-3xl`
-    const nav = `flex gap-3 md:gap-6 mr-2 md:mr-5`
-    const link = `text-sm md:text-lg font-semibold 
-                  transition-transform transition-colors duration-300 ease-in-out 
-                  hover:scale-105 active:scale-95 hover:text-indigo-300`
-    const section = `text-sm flex items-center font-medium gap-6`
-    const span = `text-rose-300 font-bold ml-1`
-    const btn = `transition-transform hover:scale-120 active:scale-95`
+                    p-5 shadow-lg shadow-indigo-700/30 md:text-3xl
+                    text-2xl font-bold w-full flex items-center justify-between `
     //
 
     const handleLogOut = async () => {
-        const {success, error} = await signOut()
-        if(error) seterror(error.message)
+        const {success} = await signOut()
         if(success) navigate("/signin")
     }
 
@@ -34,15 +23,17 @@ export default function Header(){
         <header className={header}>
             <h1>Sales Team</h1>
             {session ? 
-             <section className={section}>
+             <section className="text-sm flex items-center font-medium gap-6">
                 <p>Logged in as: 
-                    <span className={span}>{currentUser?.name},
+                    <span className="text-rose-300 font-bold ml-1">{currentUser?.name},
                         <span className="text-indigo-200 ml-1 font-small">{currentUser?.account_type}</span>
                     </span>
                 </p>
-                <button className={btn} onClick={handleLogOut}><FaSignOutAlt className="h-5 w-5" /></button>
+                <button className="transition-transform hover:scale-120 active:scale-95" onClick={handleLogOut}>
+                    <FaSignOutAlt className="h-5 w-5" />
+                    </button>
              </section> :
-             <section className={section}>
+             <section className="text-sm flex items-center font-medium gap-6">
                 <p>Sign in to see the dashboard</p>
              </section>
             }
